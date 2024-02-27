d891797c2909aa1243727177f9002dc0ed951a66
Serhii5465 <serhiiartiukh5@gmail.com>
2024-02-27 17:02:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import pydotplus
-from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
-from sklearn.model_selection import train_test_split # Import train_test_split function
-from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from sklearn.tree import DecisionTreeClassifier         # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split    # Import train_test_split function
+from sklearn import metrics                             # Import scikit-learn metrics module for accuracy calculation
 from sklearn.tree import export_graphviz
 
 def decode_label(data):
